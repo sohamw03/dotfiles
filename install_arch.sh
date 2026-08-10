31d18fef1bf45366274a0ed843c8b598ddeef914
Soham Waghmare <102748742+sohamw03@users.noreply.github.com>
2026-08-10 19:42:18 +0530

    set hardware clock to local time

--- install_arch.sh
+++ install_arch.sh
@@ -139,3 +139,5 @@ sudo sed -i '/^FallbackDNS=/d' $conf
 echo -e "[Resolve]\nDNS=1.1.1.1 1.0.0.1 8.8.8.8 8.8.4.4\nFallbackDNS=2606:4700:4700::1111 2606:4700:4700::1001 2001:4860:4860::8888 2001:4860:4860::8844" | sudo tee -a $conf
 sudo systemctl restart systemd-resolved
 
+# Set hardware to local time for dual boot setups
+timedatectl set-local-rtc 1
